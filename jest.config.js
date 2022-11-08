8242bc05b5ad5262a9340795a72dca88728b384a
Jérôme Beau <javarome@gmail.com>
2022-11-08 17:11:06 +0100

    Fix tests

--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,7 @@
+/** @type {import("ts-jest/dist/types").InitialOptionsTsJest} */
 export default {
   preset: "ts-jest/presets/default-esm",
   modulePaths: ["node_modules", "src", "."],
   testEnvironment: "node",
-  modulePathIgnorePatterns: ["dist\/.*"]
+  modulePathIgnorePatterns: ["dist\/.*", "node_modules/(?!@googlemaps)/"]
 }
--- src/util/file/HtmlFileInfo.test.ts
+++ src/util/file/HtmlFileInfo.test.ts
@@ -4,10 +4,10 @@ import {getHtmlFileInfo} from "./HtmlFileInfo"
 describe("HtmlFileInfo", () => {
 
   test("defined title", () => {
-    const fileName = "time/1/9/6/8/CondonReport/intro.htm"
+    const fileName = "src/test/test.htm"
     const fileContents = `<!--#include virtual="/header-start.html" -->
 <title>Introduction à l'édition du NCAS - Rapport Condon</title>
-<meta content="https://www.ncas.org/condon/text/intro.htm" name="url">
+<meta content="https://www.ncas.org/condon/text/test.htm" name="url">
 <meta name="author" content="Paul Jaffe (président du NCAS, janvier 1999)">
 <!--#include virtual="/header-end.html" -->`
     const context = testUtil.newHtmlContext(fileName, fileContents)
@@ -18,7 +18,7 @@ describe("HtmlFileInfo", () => {
   })
 
   test("undefined title", () => {
-    const fileName = "time/1/9/6/8/CondonReport/intro.htm"
+    const fileName = "src/test/test.htm"
     const fileContents = `<!--#include virtual="/header.html" -->
 <ul><li>Item 1</li><li>Item 2</li></ul>
 <!--#include virtual="/footer.html" -->`
