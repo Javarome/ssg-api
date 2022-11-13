8a5af4b531c5faf4e3db41de3c794b460eaf0cf0
Jérôme Beau <javarome@gmail.com>
2022-11-13 22:09:48 +0100

    Simplify jest config

--- jest.config.js
+++ jest.config.js
@@ -2,6 +2,5 @@
 export default {
   preset: "ts-jest/presets/default-esm",
   modulePaths: ["node_modules", "src", "."],
-  testEnvironment: "node",
-  modulePathIgnorePatterns: ["dist\/.*", "node_modules/(?!@googlemaps)/"]
+  testEnvironment: "node"
 }
