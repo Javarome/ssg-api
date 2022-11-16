ccaf4ac1b9d3e4b577f71a875b0e07886dafc67d
Jérôme Beau <javarome@gmail.com>
2022-11-16 08:16:35 +0100

    Add SSI echo var test

---
+++ src/step/content/replace/html/ssi/SsiEchoVarCommand.test.ts
@@ -0,0 +1,24 @@
+import {SsiEchoVarReplaceCommand} from "./SsiEchoVarCommand"
+import {RegexReplacer} from "../../RegexReplacer"
+import {HtmlSsgContext} from "../../../../../HtmlSsgContext"
+import {testUtil} from "../../../../../../test/TestUtil"
+
+describe("SsiEchoVarReplaceCommand", () => {
+
+  test("", async () => {
+    const command = new class extends SsiEchoVarReplaceCommand {
+      protected async createReplacer(context: HtmlSsgContext<any>): Promise<RegexReplacer> {
+        return {
+          replace(_match: string, ..._args: any[]): string {
+            let inputFile = context.inputFile
+            return inputFile.title || inputFile.name
+          }
+        }
+      }
+    }("title")
+    const context = testUtil.newHtmlContext("index.html",
+      `<title>RR0</title><h1 class="full-page"><!--#echo var="title" --></h1>`)
+    const output = await command.execute(context)
+    expect(output.contents).toBe(`<title>RR0</title><h1 class="full-page">RR0</h1>`)
+  })
+})
