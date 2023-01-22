9ee4eb333578f26142b269736a4f3ef11ad3408d
jerome <javarome@gmail.com>
2023-01-22 18:43:25 +0100

    Fix test

--- src/step/content/ContentStep.test.ts
+++ src/step/content/ContentStep.test.ts
@@ -31,7 +31,7 @@ describe("ContentStep", () => {
     const step = new ContentStep(contentConfigs, outputFunc)
     const context = new SsgContextImpl("fr")
     const result = await step.execute(context)
-    expect(result).toEqual({contentCount: 3})
+    expect(result).toEqual({contentCount: 4})
     const outputFile = context.outputFile as HtmlSsgFile
     expect(outputFile.meta.author).toEqual(["Jérôme Beau"])
     expect(outputFile.meta.url).toBe("https://rr0.org/tech/info/soft")
