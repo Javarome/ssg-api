7ce1dde04f6ecc23c1eb03fa0e53b356d10f8dff
javarome <javarome@gmail.com>
2025-03-16 18:38:22 +0100

    Make sure that people's title always override title inferred from first & last names

--- src/step/content/ContentStep.ts
+++ src/step/content/ContentStep.ts
@@ -82,11 +82,20 @@ export class ContentStep<C extends SsgContext = SsgContext> implements SsgStep<C
   protected async processRoot(context: C, contentsRoot: string, contentsConfig: ContentStepConfig): Promise<string[]> {
     context.debug("Processing root", contentsRoot)
     const contentFiles = await glob(contentsRoot)
+    context.setVar("contentsTotal", contentFiles.length)
     const processedFiles: string[] = []
     for (const filePath of contentFiles) {
-      let processedFile = await this.processFile(context, filePath, contentsConfig)
-      if (processedFile) {
-        processedFiles.push(processedFile)
+      Object.assign(context, {_file: {name: filePath}})
+      const lastModified = fs.statSync(context.file.name).mtime
+      Object.assign(context.file, {lastModified})
+      const doProcessFile = await this.shouldProcessFile(context, contentsConfig)
+      if (doProcessFile) {
+        let processedFile = await this.processFile(context, filePath, contentsConfig)
+        if (processedFile) {
+          processedFiles.push(processedFile)
+        }
+      } else {
+        context.debug("Skipping file", filePath)
       }
     }
     return processedFiles
@@ -107,30 +116,28 @@ export class ContentStep<C extends SsgContext = SsgContext> implements SsgStep<C
    */
   protected async processFile(context: C, filePath: string,
                               contentsConfig: ContentStepConfig): Promise<string | undefined> {
-    Object.assign(context, {_file: {name: filePath}})
-    const lastModified = fs.statSync(context.file.name).mtime
-    Object.assign(context.file, {lastModified})
-    const processFile = await this.shouldProcessFile(context, contentsConfig)
-    if (processFile) {
-      context.file = context.read(filePath)
-      const processContent = await this.shouldProcessContent(context, contentsConfig)
-      if (processContent) {
-        context.debug("Processing", filePath)
-        for (const replacement of contentsConfig.replacements) {
-          await replacement.execute(context)
-        }
-        const outputPath = contentsConfig.getOutputPath(context)
-        const output = await context.newOutput(outputPath)
-        context.debug("Writing", output.name)
-        await this.write(context, output)
-        return output.name
-      } else {
-        context.debug("Skipping content of", filePath)
-      }
+    context.debug("Processing file", filePath)
+    context.file = context.read(filePath)
+    const processContent = await this.shouldProcessContent(context, contentsConfig)
+    let outputName: string | undefined
+    if (processContent) {
+      outputName = await this.processFileContents(context, filePath, contentsConfig)
     } else {
-      context.debug("Skipping file", filePath)
+      context.debug("Not processing contents of", filePath)
     }
-    return undefined
+    return outputName
+  }
+
+  protected async processFileContents(context: C, filePath: string, contentsConfig: ContentStepConfig<SsgContext>) {
+    context.debug("Processing contents of", filePath)
+    for (const replacement of contentsConfig.replacements) {
+      await replacement.execute(context)
+    }
+    const outputPath = contentsConfig.getOutputPath(context)
+    const output = context.newOutput(outputPath)
+    context.debug("Writing new contents of", output.name)
+    await this.write(context, output)
+    return output.name
   }
 
   /**
